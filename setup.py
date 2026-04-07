7d514786c81b74e96b7da1fd8a6024b6f6c9c707
Vaishnavi Agrawal <vaishnaviagrawal2003@gmail.com>
2026-04-07 13:50:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
